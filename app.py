ffd6c4ae40c9611353a7712703e3c42b4ef3334e
Bhavyasri6825 <kadavakolla8@gmail.com>
2023-07-04 19:29:28 +0530

    changes

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import os
+os.environ['CURL_CA_BUNDLE'] = ''
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
